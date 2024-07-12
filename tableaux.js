5748a8a4015ad68ad8a5a941e458995c76fbdbc4
SerigneFallouNiang <fallouniang776@gmail.com>
2024-07-12 06:37:20 +0000

    approuver et innaprouver

--- tableaux.js
+++ tableaux.js
@@ -40,7 +40,8 @@ function AddData() {
     ideaList.push({
         libelle: libelle,
         categorie: categorie,
-        message: message
+        message: message,
+        approuver: false // Initialisez à false
     });
 
     localStorage.setItem("ideaList", JSON.stringify(ideaList));
@@ -63,8 +64,12 @@ function showData() {
         html += "<td>" + element.libelle + "</td>";
         html += "<td>" + element.categorie + "</td>";
         html += "<td>" + element.message + "</td>";
+        html += "<td>" + (element.approuver ? '✅' : '') + "</td>";
         html += `<td>
                     <button onclick="deleteData(${index})" class="btn btn-danger">Delete</button>
+                    <button onclick="approuveData(${index})" class="btn btn-primary">Approuver</button>
+                    <button onclick="nonApprouveData(${index})" class="btn btn-warning">Non approuver</button>
+
                  </td>`;
         html += "</tr>";
     });
@@ -83,4 +88,25 @@ function deleteData(index) {
     ideaList.splice(index, 1);
     localStorage.setItem("ideaList", JSON.stringify(ideaList));
     showData();
+}
+
+
+function approuveData(index) {
+    let ideaList = JSON.parse(localStorage.getItem("ideaList")) || [];
+    
+    if (ideaList[index]) {
+        ideaList[index].approuver = true;
+        localStorage.setItem("ideaList", JSON.stringify(ideaList));
+        showData(); // Rafraîchit l'affichage
+    }
+    
+}
+
+function nonApprouveData(index) {
+    let ideaList = JSON.parse(localStorage.getItem("ideaList")) || [];
+    if (ideaList[index]) {
+        ideaList[index].approuver = false;
+        localStorage.setItem("ideaList", JSON.stringify(ideaList));
+        showData(); // Rafraîchit l'affichage
+    }
 }
